65c50f205e3c8aa13d0fba63e5491479c3fd89dd
Daniel Saul <daniel@dansaul.co.uk>
2013-03-23 18:45:53 +0000

    First code commit

---
+++ firmware/kraken/kraken/kraken.ino
@@ -0,0 +1,18 @@
+/**
+  * kraken.ino
+  *
+  * Part of the Kraken Ocean Drifter
+  * http://poseidon.sgsphysics.co.uk
+  *
+  * Various Authors
+  *
+  * (C) SGS Poseidon Project 2013
+  */
+  
+  void setup(){
+    
+  }
+  
+  void loop(){
+    
+  }
