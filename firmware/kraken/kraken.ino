82798ab91fbec777284e6c5e6e14b835235f00ae
Matthew Brejza <mbrejza@gmail.com>
2013-03-27 21:56:07 +0000

    only goes into power save if has lock - fixed for seamode

--- firmware/kraken/kraken.ino
+++ firmware/kraken/kraken.ino
@@ -65,41 +65,41 @@ void loop(){
  
   while(1)
   {
-  uint8_t fx = 0;
-  for (int j = 0; j < 3; j++){
-    int32_t lat,lon,alt;
-    uint8_t b = getLocation(&lat,&lon,&alt);
-    char buf[50];
-    
-    snprintf(buf,50,"position: %li, %li, %li",lat,lon,alt);
-     Serial.println(buf);
-
-    
-    
-     uint8_t hr,mn,sc;
-     b = gps_get_time(&hr,&mn,&sc);
-     snprintf(buf,50,"time: %d:%d:%d",hr,mn,sc);
-     Serial.println(buf);
-
-    
-    
-     uint8_t lk,st;
-     b = gps_check_lock(&fx,&st);   
-     snprintf(buf,50,"lock: %d, %d",fx,st);
-     Serial.println(buf);
-     
-     delay(500);
- 
-    
-  }
-  if (fx == 3){
-    gps_sleep();
-    delay(5000);
-    delay(5000);
-    delay(5000);
-    delay(5000);
-    gps_wake();
-  }
+    uint8_t fx,st = 0;
+    for (int j = 0; j < 3; j++){
+      int32_t lat,lon,alt;
+      uint8_t b = getLocation(&lat,&lon,&alt);
+      char buf[50];
+      
+      snprintf(buf,50,"position: %li, %li, %li",lat,lon,alt);
+       Serial.println(buf);
+  
+      
+      
+       uint8_t hr,mn,sc;
+       b = gps_get_time(&hr,&mn,&sc);
+       snprintf(buf,50,"time: %d:%d:%d",hr,mn,sc);
+       Serial.println(buf);
+  
+      
+      
+       
+       b = gps_check_lock(&fx,&st);   
+       snprintf(buf,50,"lock: %d, %d",fx,st);
+       Serial.println(buf);
+       
+       delay(500);
+   
+      
+    }
+    if ((fx == 3 || fx == 2)&&(st>6)){
+      gps_sleep();
+      delay(5000);
+      delay(5000);
+      delay(5000);
+      delay(5000);
+      gps_wake();
+    }
   
   }
 
